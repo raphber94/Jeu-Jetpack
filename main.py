6d2f30acb0b7f51632af991566a65737d24f558d
Raphael <raphael.berger@efrei.net>
2022-02-17 19:12:52 +0100

    nouveau background

--- main.py
+++ main.py
@@ -7,10 +7,22 @@ pygame.init()
 
 #générer la fenetre de notre jeu
 pygame.display.set_caption("Jetpy")
-screen=pygame.display.set_mode((1027, 768))
+screen=pygame.display.set_mode((928, 793))
 
 #importer l'image du fond
 background=pygame.image.load('assets/bg6.jpg')
+brouillard1=pygame.image.load('assets/background/brouillard1.png')
+brouillard2=pygame.image.load('assets/background/brouillard2.png')
+feuillages=pygame.image.load('assets/background/feuillages.png')
+fond=pygame.image.load('assets/background/fond.png')
+herbe=pygame.image.load('assets/background/herbe.png')
+lumiere1=pygame.image.load('assets/background/lumiere1.png')
+lumiere2=pygame.image.load('assets/background/lumiere2.png')
+ombres1=pygame.image.load('assets/background/ombres1.png')
+ombres2=pygame.image.load('assets/background/ombres2.png')
+ombres3=pygame.image.load('assets/background/ombres3.png')
+terre=pygame.image.load('assets/background/terre.png')
+tronc=pygame.image.load('assets/background/tronc.png')
 
 #charger le jeu
 game=Game()
@@ -19,7 +31,7 @@ running=True
 
 #horloge
 clock = pygame.time.Clock()
-fps=120
+fps=60
 
 #coordonnées du background
 bg_x=0
@@ -29,8 +41,23 @@ bg_y=0
 while running:
 
     #appliquer arriere plan
-    bg_x-=1
-    screen.blit(background,(bg_x,bg_y))
+    #bg_x-=1
+    #screen.blit(background,(bg_x,bg_y))
+    screen.blit(fond,(0,0))
+    screen.blit(brouillard2, (0, 0))
+    screen.blit(brouillard1,(0,0))
+    screen.blit(ombres3, (0, 0))
+    screen.blit(ombres2, (0, 0))
+    screen.blit(ombres1, (0, 0))
+    screen.blit(herbe, (0, 0))
+    screen.blit(tronc, (0, 0))
+    screen.blit(terre, (0, 0))
+    screen.blit(feuillages, (0, 0))
+    screen.blit(lumiere2, (0, 0))
+    screen.blit(lumiere1, (0, 0))
+
+
+
 
     #appliquer l'image du joueur
     screen.blit(game.player.image,game.player.rect)

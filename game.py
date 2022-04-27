37b98a330d6208a0ed36a187765a115a2e1bede6
raphber94 <raphael.berger@efrei.net>
2022-04-27 19:45:01 +0200

    début niveau 1

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import pygame
 from player import Player
-from obstacles import Fire1
+from obstacles import obstacle
 
 #classe jeu
 class Game:
@@ -10,7 +10,7 @@ class Game:
         self.player=Player()
         self.all_players.add(self.player)
         self.pressed={}
-        self.all_fires = pygame.sprite.Group()
+        self.all_obstacles = pygame.sprite.Group()
 
     def check_collision(self,sprite,group):
         return pygame.sprite.spritecollide(sprite,group,False,pygame.sprite.collide_mask)
--- obstacles.py
+++ obstacles.py
@@ -1,14 +1,17 @@
 import pygame
 import game
 
-class Fire1(pygame.sprite.Sprite):
-    def __init__(self,game,x,y,type):
+class obstacle(pygame.sprite.Sprite):
+    def __init__(self,game,x,y,type,rotation):
         super().__init__()
         self.game=game
         if type=="fire":
             self.image=pygame.image.load('assets/obstacles/firetrap1.png')
         elif type=="saw":
             self.image = pygame.image.load('assets/obstacles/roue2 GRANDE.png')
+        elif type=="laser":
+            self.image = pygame.image.load('assets/obstacles/laser_grand.png')
+        self.image = pygame.transform.rotate(self.image, rotation)
         self.rect=self.image.get_rect()
         self.rect.x=x
         self.rect.y=y
@@ -16,4 +19,5 @@ class Fire1(pygame.sprite.Sprite):
 
     def move(self):
         if not self.game.check_collision(self,self.game.all_players):
-            self.rect.x-=self.velocity
+            if self.game.pressed.get(pygame.K_p) == False:
+                self.rect.x-=self.velocity

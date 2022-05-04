c4b33ee8805a0d8a7087ed3ff94883f243b39e62
Raphael <raphael.berger@efrei.net>
2022-05-04 09:14:23 +0100

    fin du niveau 1

--- niveau1.py
+++ niveau1.py
@@ -52,10 +52,12 @@ def niveau1():
     laser10 = obstacle(game, 3200, 370, "laser", -45)
     laser11 = obstacle(game, 3450, 0, "laser", -45)
     laser12 = obstacle(game, 3450, 370, "laser", 45)
+    #losange 1
     laser13 = obstacle(game, 3650, 87, "laser", -45)
     laser14 = obstacle(game, 3650, 370 - 87, "laser", 45)
     laser15 = obstacle(game, 3650 + 175, 87, "laser", 45)
     laser16 = obstacle(game, 3650 + 175, 370 - 87, "laser", -45)
+    ####
     laser17 = obstacle(game, 4100, -10, "laser", 45)
     laser18 = obstacle(game, 4100, 380, "laser", -45)
     laser19 = obstacle(game, 4300, 125, "laser", 90)
@@ -64,11 +66,77 @@ def niveau1():
     laser22 = obstacle(game, 4500, 350, "laser", 90)
     laser23 = obstacle(game, 4700, 125, "laser", 90)
     laser24 = obstacle(game, 4700, 350, "laser", 90)
+    #losange 2
     laser25 = obstacle(game, 5050, 87, "laser", -45)
     laser26 = obstacle(game, 5050, 370 - 87, "laser", 45)
     laser27 = obstacle(game, 5050+175, 87, "laser", 45)
     laser28 = obstacle(game, 5050+175, 370 - 87, "laser", -45)
-    
+    #pic vers le haut
+    laser29 = obstacle(game, 5400, 385, "laser", -45)
+    laser30 = obstacle(game, 5542, 243, "laser", -45)
+    laser31 = obstacle(game, 5684, 101, "laser", -45)
+    laser32 = obstacle(game, 5884, 101, "laser", 45)
+    laser33 = obstacle(game, 6026, 243, "laser", 45)
+    laser34 = obstacle(game, 6168, 385, "laser", 45)
+    #pic vers le bas
+    laser35 = obstacle(game, 6184, 0, "laser", 45)
+    laser36 = obstacle(game, 6326, 142, "laser", 45)
+    laser37 = obstacle(game, 6468, 284, "laser", 45)
+    laser38 = obstacle(game, 6668, 284, "laser", -45)
+    laser39 = obstacle(game, 6810, 142, "laser", -45)
+    laser40 = obstacle(game, 6952, 0, "laser", -45)
+    #petit pic vers le haut
+    laser41 = obstacle(game, 6893, 385, "laser", -45)
+    laser42 = obstacle(game, 7035, 243, "laser", -45)
+    laser43 = obstacle(game, 7235, 243, "laser", 45)
+    laser44 = obstacle(game, 7377, 385, "laser", 45)
+    #petit pic vers le bas
+    laser45 = obstacle(game, 7300, 0, "laser", 45)
+    laser46 = obstacle(game, 7442, 142, "laser", 45)
+    laser47 = obstacle(game, 7642, 142, "laser", -45)
+    laser48 = obstacle(game, 7784, 0, "laser", -45)
+    # petit pic vers le haut2
+    laser49 = obstacle(game, 7700, 385, "laser", -45)
+    laser50 = obstacle(game, 7842, 243, "laser", -45)
+    laser51 = obstacle(game, 8042, 243, "laser", 45)
+    laser52 = obstacle(game, 8184, 385, "laser", 45)
+    #croix 1
+    laser53 = obstacle(game, 8250, 0, "laser", 45)
+    laser54 = obstacle(game, 8250, 0, "laser", -45)
+    #croix 2
+    laser55 = obstacle(game, 8450, 370, "laser", 45)
+    laser56 = obstacle(game, 8450, 370,"laser", -45)
+    #croix 3
+    laser57 = obstacle(game, 8650, 150, "laser", 45)
+    laser58 = obstacle(game, 8650, 150, "laser", -45)
+    #coix 4
+    laser59 = obstacle(game, 8920, 0, "laser", 45)
+    laser60 = obstacle(game, 8920, 0, "laser", -45)
+    #croix 5
+    laser61 = obstacle(game, 9000, 370, "laser", -45)
+    laser62 = obstacle(game, 9000, 370, "laser", 45)
+    #croix 6
+    laser63 = obstacle(game, 9200, 75, "laser", 45)
+    laser64 = obstacle(game, 9200, 75, "laser", -45)
+    #croix 7
+    laser65 = obstacle(game, 9400, 300, "laser", -45)
+    laser66 = obstacle(game, 9400, 300, "laser", 45)
+    #croix 8
+    laser67 = obstacle(game, 9550, 0, "laser", 45)
+    laser68 = obstacle(game, 9550, 0, "laser", -45)
+    #croix 9
+    laser69= obstacle(game, 9650, 370, "laser", 45)
+    laser70 = obstacle(game, 9650, 370, "laser", -45)
+    #croix 10
+    laser71 = obstacle(game, 9850, 150, "laser", -45)
+    laser72 = obstacle(game, 9850, 150, "laser", 45)
+    # croix 11
+    laser73 = obstacle(game, 9850, 0, "laser", -45)
+    laser74 = obstacle(game, 9850, 0, "laser", 45)
+    # croix 12
+    laser75 = obstacle(game, 10025, 370, "laser", -45)
+    laser76 = obstacle(game, 10025, 370, "laser", 45)
+
     game.all_obstacles.add(laser)
     game.all_obstacles.add(laser2)
     game.all_obstacles.add(laser3)
@@ -97,6 +165,54 @@ def niveau1():
     game.all_obstacles.add(laser26)
     game.all_obstacles.add(laser27)
     game.all_obstacles.add(laser28)
+    game.all_obstacles.add(laser29)
+    game.all_obstacles.add(laser30)
+    game.all_obstacles.add(laser31)
+    game.all_obstacles.add(laser32)
+    game.all_obstacles.add(laser33)
+    game.all_obstacles.add(laser34)
+    game.all_obstacles.add(laser35)
+    game.all_obstacles.add(laser36)
+    game.all_obstacles.add(laser37)
+    game.all_obstacles.add(laser38)
+    game.all_obstacles.add(laser39)
+    game.all_obstacles.add(laser40)
+    game.all_obstacles.add(laser41)
+    game.all_obstacles.add(laser42)
+    game.all_obstacles.add(laser43)
+    game.all_obstacles.add(laser44)
+    game.all_obstacles.add(laser45)
+    game.all_obstacles.add(laser46)
+    game.all_obstacles.add(laser47)
+    game.all_obstacles.add(laser48)
+    game.all_obstacles.add(laser49)
+    game.all_obstacles.add(laser50)
+    game.all_obstacles.add(laser51)
+    game.all_obstacles.add(laser52)
+    game.all_obstacles.add(laser53)
+    game.all_obstacles.add(laser54)
+    game.all_obstacles.add(laser55)
+    game.all_obstacles.add(laser56)
+    game.all_obstacles.add(laser57)
+    game.all_obstacles.add(laser58)
+    game.all_obstacles.add(laser59)
+    game.all_obstacles.add(laser60)
+    game.all_obstacles.add(laser61)
+    game.all_obstacles.add(laser62)
+    game.all_obstacles.add(laser63)
+    game.all_obstacles.add(laser64)
+    game.all_obstacles.add(laser65)
+    game.all_obstacles.add(laser66)
+    game.all_obstacles.add(laser67)
+    game.all_obstacles.add(laser68)
+    game.all_obstacles.add(laser69)
+    game.all_obstacles.add(laser70)
+    game.all_obstacles.add(laser71)
+    game.all_obstacles.add(laser72)
+    game.all_obstacles.add(laser73)
+    game.all_obstacles.add(laser74)
+    game.all_obstacles.add(laser75)
+    game.all_obstacles.add(laser76)
 
     # horloge
     clock = pygame.time.Clock()
@@ -123,10 +239,32 @@ def niveau1():
 
     # boucle tant que le jeu est lancé
     while running:
+        # si le joueur rappuie sur espace alors qu'il n'appuyait pas avant on remet le temps de montée à 0
+        if up == False and game.pressed.get(pygame.K_SPACE):
+            time_up = 0
+
+        if up == True and game.pressed.get(pygame.K_SPACE) == False:
+            time_down = 0
+
+        if game.pressed.get(pygame.K_SPACE):
+            up = True
+            time_up += 1
+            v0_down = game.player.move_up(time_up, v0_up)
+        else:
+            up = False
+            if game.player.rect.y < 514:
+                print("sheshhh")
+                time_down += 1
+                v0_up = game.player.fall(time_down, v0_down)
+
+        # avec la chute libre, le joueur sort parfois de l'écran donc on le remet dans le cadre
+        if game.player.rect.y > 514:
+            game.player.rect.y = 514
 
         # appliquer arriere plan en le répétant à l'infini avec des vitesses différentes en fonction du premier plan, deuxième,...
+        # Pour tous les éléments du septième plan:
 
-        # vitesses de chaque plan:
+        #vitesses de chaque plan
         bg_seventh_plan_x -= 0.25 * 2
         bg_sixth_plan_x -= 0.5 * 2
         bg_fifth_plan_x -= 1 * 2
@@ -136,7 +274,7 @@ def niveau1():
         bg_second_plan_x -= 2 * 2
         bg_firstplan_x -= 2.2 * 2
 
-        # Pour tous les éléments du septième plan:
+
         if bg_seventh_plan_x > -1333:
             screen.blit(plan7, (bg_seventh_plan_x, bg_y))
             screen.blit(plan7, (bg_seventh_plan_x + 1333, bg_y))
@@ -209,27 +347,6 @@ def niveau1():
             if obst.rect.x<=1100 and obst.rect.x>=-200:
                 screen.blit(obst.image,obst.rect)
             obst.move()
-
-        # si le joueur rappuie sur espace alors qu'il n'appuyait pas avant on remet le temps de montée à 0
-        if up == False and game.pressed.get(pygame.K_SPACE):
-            time_up = 0
-
-        if up == True and game.pressed.get(pygame.K_SPACE) == False:
-            time_down = 0
-
-        if game.pressed.get(pygame.K_SPACE):
-            up = True
-            time_up += 1
-            v0_down = game.player.move_up(time_up, v0_up)
-        else:
-            up = False
-            if game.player.rect.y < 514:
-                time_down += 1
-                v0_up = game.player.fall(time_down, v0_down)
-
-        # avec la chute libre, le joueur sort parfois de l'écran donc on le remet dans le cadre
-        if game.player.rect.y > 514:
-            game.player.rect.y = 514
 
         # mise à jour de l'ecran
         pygame.display.flip()
